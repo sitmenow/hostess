0216323b6efaf51ccc906befe23aa42de7ca43ff
Angel Malavar <angel.malavar@gmail.com>
2019-01-25 12:32:03 -0600

    Close icon in modal

--- client/components/customers/list/ModalRequestTurn.js
+++ client/components/customers/list/ModalRequestTurn.js
@@ -5,17 +5,17 @@ import DefaultButton from "../../buttons/DefaultButton";
 import "./ModalRequestTurn.css";
 
 const ModalRequestTurn = ({ onClose, action, selectedGasStation }) => {
-  let name, plates, email_address;
+  let plates, email_address;
 
   const onSubmit = event => {
     event.preventDefault();
-    if (!name.value || !plates.value || !email_address.value) {
+    if (!plates.value || !email_address.value) {
       alert('Todos los campos son requeridos');
       return;
     }
 
     action({
-      name: name.value,
+      // name: name.value,
       plates: plates.value,
       email_address: email_address.value,
       gasStation: selectedGasStation,
@@ -24,31 +24,39 @@ const ModalRequestTurn = ({ onClose, action, selectedGasStation }) => {
     onClose();
   };
 
+  const handleClose = (event) => {
+    event.preventDefault();
+    onClose();
+  };
+
   return (
     <div className="modal">
       <form className="reservation-form" onSubmit={onSubmit}>
         <div className="modal-header">
+          <a href="" onClick={handleClose}><span className="close-icon pull-right"></span></a>
           <span className="form-title">Reserva tu espacio</span>
           <span className="form-subtitle">{selectedGasStation._name}</span>
         </div>
-        <div className="form-row">
-          <div className="form-input name">
+        {/* <div className="form-row">
+          <div className="form-input full-width">
             <span className="input-title">Nombre Completo</span>
             <input ref={node => (name = node)} type="text" name="name" />
           </div>
-        </div>
+        </div> */}
         <div className="form-row">
-          <div className="form-input people">
-            <span className="input-title">Placas</span>
-            <input ref={node => (plates = node)} type="text" name="plates" />
-          </div>
-          <div className="form-input phone">
-            <span className="input-title">Correo electrónico</span>
+          <div className="form-input full-width">
+            <span className="input-title">Correo Electrónico</span>
             <input autoCorrect="off" autoCapitalize="none" ref={node => (email_address = node)} type="email" name="email" />
           </div>
         </div>
+        <div className="form-row">
+          <div className="form-input width-60">
+            <span className="input-title">Placas</span>
+            <input ref={node => (plates = node)} type="text" name="plates" />
+          </div>
+        </div>
         <div className="modal-footer">
-          <DefaultButton onClickHandler={onClose} text="Cancelar" style="red" />
+          {/* <DefaultButton onClickHandler={onClose} text="Cancelar" style="red" /> */}
           <DefaultButton onClickHandler={action} text="Solicitar" style="blue" />
         </div>
       </form>
