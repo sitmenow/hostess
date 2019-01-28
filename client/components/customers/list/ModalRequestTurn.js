97c59f900c01c96e002a4b1dc045d2481b259fbc
Angel Malavar <angel.malavar@gmail.com>
2019-01-27 21:31:51 -0600

    Fixing and styling modal

--- client/components/customers/list/ModalRequestTurn.js
+++ client/components/customers/list/ModalRequestTurn.js
@@ -1,60 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import DefaultButton from "../../buttons/DefaultButton";
 import "./ModalRequestTurn.css";
 
-const ModalRequestTurn = ({ onClose, action, selectedGasStation }) => {
-  let plates, email_address;
+class ModalRequestTurn extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
 
-  const onSubmit = event => {
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside);
+    document.addEventListener('touchend', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside);
+    document.removeEventListener('touchend', this.handleClickOutside);
+  }
+
+  onSubmit(event) {
     event.preventDefault();
-    if (!plates.value || !email_address.value) {
+    if (!this.plates.value || !this.email_address.value) {
       alert('Todos los campos son requeridos');
       return;
     }
-    action({
-      plates: plates.value,
-      email_address: email_address.value,
-      gasStation: selectedGasStation,
+    this.props.action({
+      plates: this.plates.value,
+      // TODO: Verify backend if it's enabled to accept email property
+      name: this.email_address.value,
+      // email_address: email_address.value,
+      gasStation: this.props.selectedGasStation,
     });
 
-    onClose();
-  };
+    this.onClose();
+  }
 
-  const handleClose = (event) => {
+  handleClose(event) {
     event.preventDefault();
-    onClose();
-  };
+    this.props.onClose();
+  }
 
-  return (
-    <div className="modal">
-      <form className="reservation-form" onSubmit={onSubmit}>
-        <div className="modal-header">
-          <a href="" onClick={handleClose}><span className="close-icon pull-right"></span></a>
-          <span className="form-title">Reserva tu espacio</span>
-          <span className="form-subtitle">{selectedGasStation._name}</span>
-        </div>
-        <div className="form-row">
-          <div className="form-input full-width">
-            <span className="input-title">Correo Electrónico</span>
-            <input autoCorrect="off" autoCapitalize="none" ref={node => (email_address = node)} type="email" name="email" />
+  handleClickOutside(event) {
+    if (event.target.id === 'modalContainer') {
+      this.handleClose(event);
+    }
+  }
+
+  render() {
+    return(
+      <div className="modal" id="modalContainer">
+        <form className="reservation-form" onSubmit={this.onSubmit}>
+          <div className="modal-header">
+            <a href="" onClick={this.handleClose}><span className="close-icon pull-right"></span></a>
+            <span className="form-title">Reserva tu espacio</span>
+            <span className="form-subtitle">{this.props.selectedGasStation._name}</span>
           </div>
-        </div>
-        <div className="form-row">
-          <div className="form-input width-60">
-            <span className="input-title">Placas</span>
-            <input ref={node => (plates = node)} type="text" name="plates" />
+          <div className="form-row">
+            <div className="form-input full-width">
+              <span className="input-title">Correo Electrónico</span>
+              <input autoCorrect="off" autoCapitalize="none" ref={node => (this.email_address = node)} type="email" name="email" />
+            </div>
           </div>
-        </div>
-        <div className="modal-footer">
-          {/* <DefaultButton onClickHandler={onClose} text="Cancelar" style="red" /> */}
-          <DefaultButton onClickHandler={onSubmit} text="Solicitar" style="blue" />
-        </div>
-      </form>
-    </div>
-  );
-};
+          <div className="form-row">
+            <div className="form-input width-60">
+              <span className="input-title">Placas</span>
+              <input ref={node => (this.plates = node)} type="text" name="plates" />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <DefaultButton onClickHandler={this.onSubmit} text="Solicitar" style="blue" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 ModalRequestTurn.propTypes = {
   selectedGasStation: PropTypes.object,
