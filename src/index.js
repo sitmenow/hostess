dfaee98e89ecd6b31c982b58c583743b1417fa95
emmanueldiaz <cdiaz1594@gmail.com>
2019-01-28 09:48:28 -0600

    Integrate Auth0 WIP

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import thunkMiddleware from "redux-thunk";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import rootReducer from "./reducers";
 import { getTurnsIfNeeded } from "./actions";
 import { saveState } from "./localStorage";
+import Auth from "./Auth/Auth";
 
 import DashboardContainer from "./components/dashboard/DashboardContainer.js";
 import Header from "./components/Header";
 
 import "./index.css";
 
+const auth = new Auth();
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 store.subscribe(() => {
@@ -21,15 +24,18 @@ store.subscribe(() => {
 class App extends React.Component {
   componentDidMount() {
     store.dispatch(getTurnsIfNeeded());
+    const { isAuthenticated } = auth;
+    console.log("here", isAuthenticated);
+    if (!isAuthenticated()) auth.login();
   }
 
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Header />
-          <DashboardContainer />
-        </div>
+        <Header />
+        <Router>
+          <Route path="/" component={DashboardContainer} />
+        </Router>
       </Provider>
     );
   }
